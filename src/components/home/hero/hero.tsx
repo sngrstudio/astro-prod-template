1cc307c17af0ebaace27227d3bdc77aa75124c1e
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2022-11-02 16:20:54 +0000

    replace main content with README

--- src/components/home/hero/hero.tsx
+++ src/components/home/hero/hero.tsx
@@ -1,28 +1,9 @@
-import type { FC } from 'react'
+import type { FC, PropsWithChildren } from 'react'
 import style from './hero.module.scss'
 
-const Hero: FC = () => (
-  <section className="hero min-h-screen">
-    <div className="hero-content">
-      <div className="max-w-md">
-        <h1 className={style.h1}>A template for production</h1>
-        <p className="text-center mb-2">
-          The template is created using{' '}
-          <a className="link-hover" href="https://astro.build" rel="nofollow">
-            Astro
-          </a>{' '}
-          and a pinch of React. Batteries included: SASS, TailwindCSS + DaisyUI,
-          PostCSS with Autoprefixer and Font Magician plugin.
-        </p>
-        <p className="text-center italic text-sm">
-          a{' '}
-          <a className="link-hover" href="http://sngr.studio/">
-            SNGR Studio
-          </a>{' '}
-          tool. (c) {new Date().getFullYear()}
-        </p>
-      </div>
-    </div>
+const Hero: FC<PropsWithChildren> = ({ children }) => (
+  <section className={style.section}>
+    <div className={`prose ${style.content}`}>{children}</div>
   </section>
 )
 
