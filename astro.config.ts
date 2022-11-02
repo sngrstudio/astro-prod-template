83668ac2218adda0dfaf80e95d31d822387ad153
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2022-11-02 12:14:31 +0000

    replace icon library, update tsconfig

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
+import Icons from 'unplugin-icons/vite'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   vite: {
-    ssr: {
-      external: ['svgo'],
-    },
+    plugins: [Icons({ compiler: 'jsx', jsx: 'react' })],
   },
 })
