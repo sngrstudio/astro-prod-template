4a004c153fb68cc64eef7ab6207a5143f5f4c87b
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2023-04-05 00:56:14 +0000

    implement simple homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 import { defineConfig } from 'astro/config'
+import Yaml from '@modyfi/vite-plugin-yaml'
 
-import react from '@astrojs/react'
+import compress from 'astro-compress'
+import critters from 'astro-critters'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://sngrstudio-astro-template.netlify.app',
+  integrations: [compress(), critters()],
+  vite: {
+    plugins: [Yaml()],
+  },
+  experimental: {
+    assets: true,
+  },
+  image: {
+    service: 'astro/assets/services/sharp',
+  },
 })
